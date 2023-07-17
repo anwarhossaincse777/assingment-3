062d069731b0beb16eb0dd3fc448dd69920df5fb
Betabloker <72620275+Betabloker@users.noreply.github.com>
2023-07-18 00:36:45 +0600

    after finish

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import { createApp } from 'vue'
+
+
+// <!-- Vendor CSS Files -->
+import './assets/vendor/bootstrap-icons/bootstrap-icons.css'
+import './assets/vendor/glightbox/css/glightbox.min.css'
+import './assets/vendor/swiper/swiper-bundle.min.css'
+import './assets/vendor/aos/aos.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// <!-- Template Main CSS File -->
+import './assets/css/main.css'
+
+// <!-- Vendor JS Files -->
+
+import './assets/vendor/bootstrap/js/bootstrap.bundle.min.js'
+import './assets/vendor/glightbox/js/glightbox.min.js'
+import './assets/vendor/purecounter/purecounter_vanilla.js'
+import './assets/vendor/isotope-layout/isotope.pkgd.min.js'
+import './assets/vendor/swiper/swiper-bundle.min.js'
+// import './assets/vendor/aos/aos.js'
+
+// <!-- Template Main JS File -->
+import './assets/js/main.js'
+
+import App from './App.vue'
+
+createApp(App).mount('#app')
